2b78696b9c7f2b67f90ccb13d448eb3c48e0184a
Vlad Lazin <vladlazin@gmail.com>
2022-09-12 19:02:46 +0200

    Compiled plug-in

---
+++ RadicalLLSample/Plugins/radical-unreal-livelink-plugin/Intermediate/Build/Win64/UE4Editor/Inc/UnrealLiveLinkRemapPlugin/RadToEpicRemapAsset.gen.cpp
@@ -0,0 +1,85 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "UnrealLiveLinkRemapPlugin/Public/RadToEpicRemapAsset.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeRadToEpicRemapAsset() {}
+// Cross Module References
+	UNREALLIVELINKREMAPPLUGIN_API UClass* Z_Construct_UClass_URadToEpicRemapAsset_NoRegister();
+	UNREALLIVELINKREMAPPLUGIN_API UClass* Z_Construct_UClass_URadToEpicRemapAsset();
+	UNREALLIVELINKREMAPPLUGIN_API UClass* Z_Construct_UClass_URadicalLiveLinkRemapAssetBase();
+	UPackage* Z_Construct_UPackage__Script_UnrealLiveLinkRemapPlugin();
+// End Cross Module References
+	void URadToEpicRemapAsset::StaticRegisterNativesURadToEpicRemapAsset()
+	{
+	}
+	UClass* Z_Construct_UClass_URadToEpicRemapAsset_NoRegister()
+	{
+		return URadToEpicRemapAsset::StaticClass();
+	}
+	struct Z_Construct_UClass_URadToEpicRemapAsset_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_URadToEpicRemapAsset_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_URadicalLiveLinkRemapAssetBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_UnrealLiveLinkRemapPlugin,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_URadToEpicRemapAsset_Statics::Class_MetaDataParams[] = {
+		{ "Comment", "/**\n * \n */" },
+		{ "IncludePath", "RadToEpicRemapAsset.h" },
+		{ "ModuleRelativePath", "Public/RadToEpicRemapAsset.h" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_URadToEpicRemapAsset_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<URadToEpicRemapAsset>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_URadToEpicRemapAsset_Statics::ClassParams = {
+		&URadToEpicRemapAsset::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x001000A0u,
+		METADATA_PARAMS(Z_Construct_UClass_URadToEpicRemapAsset_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_URadToEpicRemapAsset_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_URadToEpicRemapAsset()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_URadToEpicRemapAsset_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(URadToEpicRemapAsset, 1327463163);
+	template<> UNREALLIVELINKREMAPPLUGIN_API UClass* StaticClass<URadToEpicRemapAsset>()
+	{
+		return URadToEpicRemapAsset::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_URadToEpicRemapAsset(Z_Construct_UClass_URadToEpicRemapAsset, &URadToEpicRemapAsset::StaticClass, TEXT("/Script/UnrealLiveLinkRemapPlugin"), TEXT("URadToEpicRemapAsset"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(URadToEpicRemapAsset);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ RadicalLLSample/Plugins/radical-unreal-livelink-plugin/Intermediate/Build/Win64/UE4Editor/Inc/UnrealLiveLinkRemapPlugin/RadicalLiveLinkRemapAssetBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef UNREALLIVELINKREMAPPLUGIN_RadicalLiveLinkRemapAssetBase_generated_h
+#error "RadicalLiveLinkRemapAssetBase.generated.h already included, missing '#pragma once' in RadicalLiveLinkRemapAssetBase.h"
+#endif
+#define UNREALLIVELINKREMAPPLUGIN_RadicalLiveLinkRemapAssetBase_generated_h
+
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_SPARSE_DATA
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_RPC_WRAPPERS
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesURadicalLiveLinkRemapAssetBase(); \
+	friend struct Z_Construct_UClass_URadicalLiveLinkRemapAssetBase_Statics; \
+public: \
+	DECLARE_CLASS(URadicalLiveLinkRemapAssetBase, ULiveLinkRemapAsset, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/UnrealLiveLinkRemapPlugin"), NO_API) \
+	DECLARE_SERIALIZER(URadicalLiveLinkRemapAssetBase)
+
+
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesURadicalLiveLinkRemapAssetBase(); \
+	friend struct Z_Construct_UClass_URadicalLiveLinkRemapAssetBase_Statics; \
+public: \
+	DECLARE_CLASS(URadicalLiveLinkRemapAssetBase, ULiveLinkRemapAsset, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/UnrealLiveLinkRemapPlugin"), NO_API) \
+	DECLARE_SERIALIZER(URadicalLiveLinkRemapAssetBase)
+
+
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API URadicalLiveLinkRemapAssetBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(URadicalLiveLinkRemapAssetBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, URadicalLiveLinkRemapAssetBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(URadicalLiveLinkRemapAssetBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API URadicalLiveLinkRemapAssetBase(URadicalLiveLinkRemapAssetBase&&); \
+	NO_API URadicalLiveLinkRemapAssetBase(const URadicalLiveLinkRemapAssetBase&); \
+public:
+
+
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API URadicalLiveLinkRemapAssetBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API URadicalLiveLinkRemapAssetBase(URadicalLiveLinkRemapAssetBase&&); \
+	NO_API URadicalLiveLinkRemapAssetBase(const URadicalLiveLinkRemapAssetBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, URadicalLiveLinkRemapAssetBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(URadicalLiveLinkRemapAssetBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(URadicalLiveLinkRemapAssetBase)
+
+
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_12_PROLOG
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_SPARSE_DATA \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_RPC_WRAPPERS \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_INCLASS \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_SPARSE_DATA \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_INCLASS_NO_PURE_DECLS \
+	RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> UNREALLIVELINKREMAPPLUGIN_API UClass* StaticClass<class URadicalLiveLinkRemapAssetBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID RadicalLLSample_Plugins_radical_unreal_livelink_plugin_Source_UnrealLiveLinkRemapPlugin_Public_RadicalLiveLinkRemapAssetBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
